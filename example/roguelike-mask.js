cac11d758c71e1d45797f199190078ffd058634b
Marak <marak.squires@gmail.com>
2024-01-24 23:07:02 -0500

    Adds `TileMap.use(subMap)` Adds `TileMap.mask()` Adds default rogue-like charset

---
+++ example/roguelike-mask.js
@@ -0,0 +1,27 @@
+import daedalus from '../lib/daedalus.js';
+import generateMap from '../lib/mazes/RecursiveBacktrack.js';
+
+let map = new daedalus.TileMap({
+  width: 64,
+  height: 32,
+  tileWidth: 16,
+  tileHeight: 16
+});
+
+console.log('map', map)
+console.log('random', map.random())
+
+daedalus.terrains.FaultLine(map, {});
+
+// normalize to tile set index
+map.normalizeToTileSetIndex(4);
+
+console.log('map', map)
+
+// default mask
+console.log(map.mask())
+
+// custom mask
+console.log(map.mask([
+  '-', '#', '+', '0'
+]))
---
+++ example/sub-maps.js
@@ -0,0 +1,25 @@
+import daedalus from '../lib/daedalus.js';
+import generateMap from '../lib/mazes/RecursiveBacktrack.js';
+
+// Main map
+const mainMap = new daedalus.TileMap({ width: 8, height: 8, tileWidth: 16, tileHeight: 16 });
+
+// Submaps
+const subMap1 = new daedalus.TileMap({ width: 4, height: 4, tileWidth: 16, tileHeight: 16 });
+const subMap2 = new daedalus.TileMap({ width: 4, height: 4, tileWidth: 16, tileHeight: 16 });
+const subMap3 = new daedalus.TileMap({ width: 4, height: 4, tileWidth: 16, tileHeight: 16 });
+const subMap4 = new daedalus.TileMap({ width: 4, height: 4, tileWidth: 16, tileHeight: 16 });
+
+// Fill each submap with distinct values
+subMap1.fill(1);
+subMap2.fill(2);
+subMap3.fill(3);
+subMap4.fill(4);
+
+// Position submaps in each quadrant of the main map
+mainMap.use(subMap1, 0, 0); // Top-left quadrant
+mainMap.use(subMap2, 4, 0); // Top-right quadrant
+mainMap.use(subMap3, 0, 4); // Bottom-left quadrant
+mainMap.use(subMap4, 4, 4); // Bottom-right quadrant
+
+console.log('mainMap', mainMap);
